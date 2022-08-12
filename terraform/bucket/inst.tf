420b20b36f1eb9be17d35cc95b52aea5b4feba2f
Oleksandr Rudenko <oleksandr.r@quali.com>
2022-08-12 15:36:53 +0300

    add inst

--- terraform/bucket/inst.tf
+++ terraform/bucket/inst.tf
@@ -1,24 +1,17 @@
-resource "google_compute_instance" "default" {
-  name         = "test-${var.SANDBOX_ID}"
-  machine_type = "e2-medium"
-  zone         = var.zone
+data "google_compute_instance_template" "generic" {
+  name    = "instance-template-1"
+  project = "alexs-project-239406"
+}
 
-  tags = ["foo", "bar"]
 
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-    }
+resource "google_compute_instance_from_template" "tpl" {
+  name = "test-${var.SANDBOX_ID}"
+  
+  source_instance_template = data.google_compute_instance_template.generic.id
+
+  // Override fields from instance template
+  can_ip_forward = false
+  labels = {
+    my_key = "my_value"
   }
-
-  // Local SSD disk
-
-  network_interface {
-    network = "default"
-  }
-
-  metadata = {
-    foo = "bar"
-  }
-
 }
